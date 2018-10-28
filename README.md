87803eb6628d1332306e6abad25f363a513e10a4
Anita F. McFarland <anitam2345@yahoo.com>
2018-10-28 16:57:37 -0700

    git commit-m"readme update"

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# liri-bot
+## Liri-bot
+**Version 1.0.0**
+
+
+## Overview of project
+LIRI is a command line node app that is a Language Interpretation and Recognition Interface.
+1.  LIRI will search Spotify for songs,Bands in Town for concerts, and OMDB for movies, and Spotify for songs.
+2.  API requests for Node-Spotify-API, Bands in town, and OMDB.
+3.  The program will run in node.js and will need screenshots to show how it runs.
+
+Files needed: 
+*liri.js 
+*key.js
+*gitignore
+*random.txt
+*.env 
+*ReadMe
+*package.json
+
+
+
+
+@ Anita McFarland
--- liri.js
+++ liri.js
@@ -12,39 +12,54 @@ const input = process.argv[3];
 
 //const spotify = new Spotify(keys.spotify);
 console.log("KEYS", keys);
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: "d63442d2266e4fa5b56d6f503b3397a1",
+  secret: "f1aba38db6c24267a02301d55e1b526et",
+});
+ 
+  var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp".then(function(data) {
+    console.log(data)
+ })
 
+  var queryUrl =  "http://www.omdbapi.com/?t=" + "Title" + "&y=&plot=short&IMDBRating&lang&actors&country&apikey=f1aba38db6c24267a02301d55e1b526e"
+  .then(function(data) {
+    console.log(data)
+ })
 
-var queryUrl = "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10" + artist + songName + preview + album;
+    request(queryUrl, function(error, response, body){
 
-var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
-
-var queryUrl =  "http://www.omdbapi.com/?t=" + "Title" + "&y=&plot=short&IMDBRating&lang&actors&country&apikey=f1aba38db6c24267a02301d55e1b526e";
-request(queryUrl, function(error, response, body){
-
-if (!error && response.statusCode === 200) {
+    if (!error && response.statusCode === 200) {
      
     // Parse the body of the site and recover just the imdbRating
     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
     console.log("Release Year: " + JSON.parse(body).Year);
   }
 
-// request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", 
+ inquirer.prompt([{
 
-
-switch(action){
-    case 'concert-this':
-    console.log("You want a concert for ", input);
-    break;
-    case 'spotify-this-song':
-    console.log("You what a song for ", input);
-    break;
-    case 'movie-this':
-    console.log("You want movie info on ", input);
-    break;
-    case 'do-what-it-says':
-    console.log("Do what it says");
-    break;
-    default:
-    console.log("No idea what you are asking for...");
-
-}});
+    type:"input",
+    name:"userChoice",
+    messge: "Please select an option!",
+    choices: ["concert-this","spotify-this-song","movie-this","do-what-it-says"]
+}
+])
+    function checkArg(argument,input){
+    switch(action){
+       case 'concert-this':
+       console.log("You want a concert for ", input);
+       break;
+       case 'spotify-this-song':
+       console.log("You what a song for ", input);
+       break;
+       case 'movie-this':
+       console.log("You want movie info on ", input);
+       break;
+       case 'do-what-it-says':
+       console.log("Do what it says");
+       break;
+       default:
+       console.log("No idea what you are asking for...");
+}
+}};
